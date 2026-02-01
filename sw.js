60b032b310c5f273a2df114ce200e1be7f58c356
yspark <aidronelab@ysparkui-MacBookPro.local>
2026-02-01 20:14:49 +0900

    Initial deploy

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'cc-v1';
+const BASE = self.location.pathname.replace(/sw\.js$/, '');
+const ASSETS = [
+  BASE,
+  BASE + 'index.html',
+  BASE + 'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
